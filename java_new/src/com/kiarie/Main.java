629ccae1dfd0fbd9d8dbfbadd8498a9f275f8b1c
kevin <kiariekevin22@gmail.com>
2020-12-29 12:34:40 +0300

    number format and Scanner

--- java_new/src/com/kiarie/Main.java
+++ java_new/src/com/kiarie/Main.java
@@ -2,8 +2,10 @@ package com.kiarie;
 
 import java.awt.*;
 import java.lang.reflect.Array;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
 
@@ -20,30 +22,60 @@ public class Main {
 //        multiDimArr[1][0]=8;
 //	    System.out.println(Arrays.deepToString(multiDimArr));
 
-	    int x = 1;
-	    x += 3;
-        System.out.println(x);
+//	    int x = 1;
+//	    x += 3;
+//        System.out.println(x);
+//
+//         //order of operation
+//        int y = 40;
+//        x=(y-4)/12;
+//        System.out.println(x);
+//
+//        //implicit casting or automatic conversion
+//        //byte < short < int < long < float < double
+//        short i=5;
+//        int j = i+7;
+//        System.out.println(j);
+//
+//        //expicit casting or manual conversion
+//        float q = 9.9f;
+//        int w = (int)q + 6;
+//        System.out.println(w);
+//
+//        //wrapper classes e.g Integer for type conversion
+//        String t = "121";
+//        int x2 = Integer.parseInt(t)/11;
+//        System.out.println(x2);
+//
+//        //Math Class
+//        float y2 = 6.7f;
+//        int result = Math.round(y2);//7
+//        int ceiling = (int) Math.ceil(8.1); //9
+//        double floor1 = Math.floor(8.8);//8
+//        System.out.println(floor1);
+//        //random integer
+//        int f = (int)(Math.random()*10);
+//        System.out.println(f);
 
-         //order of operation
-        int y = 40;
-        x=(y-4)/12;
-        System.out.println(x);
+        //number fomating
+//        NumberFormat currency = NumberFormat.getCurrencyInstance();
+//        String result2 = currency.format(345098754.89786);
+//        System.out.println(result2);
+//
+//        NumberFormat percentage = NumberFormat.getPercentInstance();
+//        String percentResult = percentage.format(0.05);
+//        System.out.println(percentResult);
 
-        //implicit casting or automatic conversion
-        //byte < short < int < long < float < double
-        short i=5;
-        int j = i+7;
-        System.out.println(j);
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter your age: ");
+//        int age = scanner.nextInt();
+//        System.out.println("Your current age is: "+age);
 
-        //expicit casting or manual conversion
-        float q = 9.9f;
-        int w = (int)q + 6;
-        System.out.println(w);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Your name is: "+name);
 
-        //wrapper classes e.g Integer for type conversion
-        String t = "121";
-        int x2 = Integer.parseInt(t)/11;
-        System.out.println(x2);
+
 
 
 
